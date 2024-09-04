ceac21b17e2e47b1a57844d08bd547d9ffed4374
rhribeiro25 <renan.shaolin.25@gmail.com>
2024-09-04 21:13:12 +0100

    starting new kafka project

--- src/main/resources/db/migration/V1__create_order_table.sql
+++ src/main/resources/db/migration/V1__create_order_table.sql
@@ -2,10 +2,11 @@ CREATE TABLE IF NOT EXISTS promotions (
   id bigint GENERATED ALWAYS AS identity
   	CONSTRAINT pk_order
   		primary key,
-  		name varchar(124) NOT NULL
-  discount_percentage bigint NOT NULL,
+  		name varchar(127) NOT NULL
+  name varchar(255) NOT null
   product_id bigint NOT NULL,
+  discount_percentage bigint NOT NULL,
   start_date date NOT NULL,
   finish_date date NOT NULL,
-  status varchar(255) NOT NULL
+  status varchar(127) NOT NULL
 );
